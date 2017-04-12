c8dbd69d9d97edead89a69c34db4d5a2f458c887
chuxiuhong <nanwugongzi@vip.qq.com>
2017-04-12 22:03:11 +0800

    complete a cos similar Password compare code

--- src/com/chuxiuhong/judge/PeoplePassword.java
+++ src/com/chuxiuhong/judge/PeoplePassword.java
@@ -27,33 +27,39 @@ public class PeoplePassword extends People {
         int[] startTime = new int[len];
         int cur = 0;
         float deltaTime = 0;
-        for (int i = 0; i < charList.length; i++) {
-            if (password.charAt(i) != '_') {
+        int i = 0;
+        while (i < charList.length && cur < len) {
+//            System.out.println("i: "+i);
+//            System.out.println("cur " +cur);
+            if (password.charAt(cur) != '_') {
                 if (charList[i] == (int) password.charAt(cur) && stateList[i] == 0) {
                     startTime[cur] = timeList[i];
                     pressTime[cur] = timeList[i];
                     for (int j = i; j < charList.length; j++) {
-                        if (charList[j] == (int) password.charAt(cur) && stateList[i] == 1) {
+                        if (charList[j] == (int) password.charAt(cur) && stateList[j] == 1) {
                             deltaTime = timeList[j] - pressTime[cur];
                             pressTime[cur] = deltaTime;
+                            break;
                         }
                     }
                     cur++;
                 }
             } else {
-                if (charList[i] == 189 && password.charAt(cur) == '_' && stateList[i] == 0) {
+                if (charList[i] == 189 && stateList[i] == 0) {
                     startTime[cur] = timeList[i];
                     pressTime[cur] = timeList[i];
                     for (int j = i; j < charList.length; j++) {
-                        if (charList[i] == 189 && password.charAt(cur) == '_' && stateList[i] == 1) {
+                        if (charList[j] == 189 && stateList[j] == 1) {
                             pressTime[cur] = timeList[j] - pressTime[cur];
+                            break;
                         }
                     }
                     cur++;
                 }
             }
+            i++;
         }
-        for (int i = 0; i < len - 1; i++) {
+        for (i = 0; i < len - 1; i++) {
             flightTime[i] = startTime[i + 1] - pressTime[i] - startTime[i];
         }
         return new PwdFeature(pressTime, flightTime);
@@ -78,13 +84,13 @@ public class PeoplePassword extends People {
             maxFlightTime[i] = -100;
             minFlightTime[i] = 100000;
         }
-        for (int i = 1; i < pp.length; i++) {
+        for (int i = 0; i < pp.length; i++) {
             for (int j = 0; j < len; j++) {
                 pressTime[j] += PeoplePassword.getFeature(pp[i], password).getPressTime()[j];
                 if (PeoplePassword.getFeature(pp[i], password).getPressTime()[j] > maxPressTime[j]) {
                     maxPressTime[j] = PeoplePassword.getFeature(pp[i], password).getPressTime()[j];
                 }
-                if (PeoplePassword.getFeature(pp[i], password).getPressTime()[j]< minPressTime[j]) {
+                if (PeoplePassword.getFeature(pp[i], password).getPressTime()[j] < minPressTime[j]) {
                     minPressTime[j] = PeoplePassword.getFeature(pp[i], password).getPressTime()[j];
                 }
             }
@@ -114,15 +120,30 @@ public class PeoplePassword extends People {
         for (int i = 0; i < len - 1; i++) {
             temp = 0;
             for (int j = 0; j < pp.length; j++) {
+//                System.out.println("flightTime[i] " + flightTime[i]);
+//                System.out.println("PeoplePassword.getFeature(pp[j], password).getFlightTime()[i] " + PeoplePassword.getFeature(pp[j], password).getFlightTime()[i]);
                 temp += Math.pow((flightTime[i] - PeoplePassword.getFeature(pp[j], password).getFlightTime()[i]), 2);
             }
             varFlight[i] = temp / pp.length;
+            //System.out.println("varFlight[i] " + varFlight[i]);
         }
         return new PwdFeature(pressTime, flightTime, maxPressTime, minPressTime, maxFlightTime, minFlightTime, varPress, varFlight);
     }
 
     public static void main(String[] args) {
-        String a = "abBB-_234";
-        System.out.println((int) a.toUpperCase().charAt(0));
+        int[] train_char1 = {65, 65, 80, 83, 80, 83, 76, 76, 76, 76, 75, 75, 81, 87, 81, 87, 49, 50, 49, 50};
+        int[] train_state1 = {0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1};
+        int[] train_time1 = {0, 119, 141, 209, 228, 299, 359, 419, 479, 549, 629, 699, 929, 1039, 1090, 1149, 1229, 1309, 1369, 1419};
+        PeoplePassword p1 = new PeoplePassword(train_char1, train_state1, train_time1);
+        System.out.println("press ");
+        for (float i : PeoplePassword.getFeature(p1, "apsllkqw12").getPressTime()
+                ) {
+            System.out.print(" " + i);
+        }
+        System.out.println("\nflight");
+        for (float i : PeoplePassword.getFeature(p1, "apsllkqw12").getFlightTime()
+                ) {
+            System.out.print(" " + i);
+        }
     }
 }
