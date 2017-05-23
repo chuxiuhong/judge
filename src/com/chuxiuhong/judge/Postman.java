e1b4d5b5aa7d7d46d3ebbf83deda3fc0e122ecc9
chuxiuhong <nanwugongzi@vip.qq.com>
2017-05-23 14:30:43 +0800

    fix bug

--- src/com/chuxiuhong/judge/Postman.java
+++ src/com/chuxiuhong/judge/Postman.java
@@ -9,9 +9,9 @@ import java.util.HashMap;
 
 public class Postman {
     /**
-     * @param test    为前端发送数据
-     * @param train    数据库内数据
-     * @param pass 密码明文
+     * @param test  为前端发送数据
+     * @param train 数据库内数据
+     * @param pass  密码明文
      * @return 相似度，float型，取值[0,1]
      */
     public static float comparePass(String test, String train, String pass) {
@@ -64,52 +64,6 @@ public class Postman {
             }
         }
         try {
-//            System.out.println("aCharList = ");
-//            for (int i : aCharList
-//                    ) {
-//                System.out.print(i + " ");
-//            }
-//            System.out.println("\naStateList = ");
-//            for (int i : aStateList
-//                    ) {
-//                System.out.print(i + " ");
-//            }
-//            System.out.println("\naTimeList = ");
-//            for (int i : aTimeList
-//                    ) {
-//                System.out.print(i + " ");
-//            }
-//            System.out.println("\nbCharList1 = ");
-//            for (int i : bCharList1
-//                    ) {
-//                System.out.print(i + " ");
-//            }
-//            System.out.println("\nbStateList1 = ");
-//            for (int i : bStateList1
-//                    ) {
-//                System.out.print(i + " ");
-//            }
-//            System.out.println("\nbTimeList1 = ");
-//            for (int i : bTimeList1
-//                    ) {
-//                System.out.print(i + " ");
-//            }
-//            System.out.println("\nbCharList2 = ");
-//            for (int i : bCharList2
-//                    ) {
-//                System.out.print(i + " ");
-//            }
-//            System.out.println("\nbStateList2 = ");
-//            for (int i : bStateList2
-//                    ) {
-//                System.out.print(i + " ");
-//            }
-//            System.out.println("\nbTimeList2 = ");
-//            for (int i : bTimeList2
-//                    ) {
-//                System.out.print(i + " ");
-//            }
-//            System.out.println("\npass "+pass);
             PeoplePassword aPwd = new PeoplePassword(aCharList, aStateList, aTimeList);
             PeoplePassword bPwd1 = new PeoplePassword(bCharList1, bStateList1, bTimeList1);
             PeoplePassword bPwd2 = new PeoplePassword(bCharList2, bStateList2, bTimeList2);
@@ -121,7 +75,8 @@ public class Postman {
             return 0.79f;
         }
     }
-    public static float compareText(String test,String train) {
+
+    public static float compareText(String test, String train) {
         String[] aList = test.split("\n|,");
         String[] bList = train.split("\n|,");
         int aLength = aList.length;
@@ -170,24 +125,21 @@ public class Postman {
                 }
             }
         }
-        HashMap<Integer,Integer> filter= new HashMap<>();
+        HashMap<Integer, Integer> filter = new HashMap<>();
         int[] flagList = new int[aCharList.size()];
         for (int i = 0; i < aCharList.size(); i++) {
-            if (aStateList.get(i) == 0 && !filter.containsKey(i)){
-                filter.put(aCharList.get(i),i);
-            }
-            else if(aStateList.get(i) == 0){
+            if (aStateList.get(i) == 0 && !filter.containsKey(i)) {
+                filter.put(aCharList.get(i), i);
+            } else if (aStateList.get(i) == 0) {
                 flagList[i] = 1;
-            }
-            else if (aStateList.get(i) == 1 && filter.containsKey(i)){
+            } else if (aStateList.get(i) == 1 && !filter.containsKey(i)) {
                 filter.remove(aCharList.get(i));
-            }
-            else if(aStateList.get(i)==1){
+            } else if (aStateList.get(i) == 1) {
                 flagList[i] = 1;
             }
         }
         for (int i = flagList.length - 1; i >= 0; i--) {
-            if (flagList[i] == 1){
+            if (flagList[i] == 1) {
                 aCharList.remove(i);
                 aTimeList.remove(i);
                 aStateList.remove(i);
@@ -196,21 +148,18 @@ public class Postman {
         filter.clear();
         int[] flagList1 = new int[bCharList1.size()];
         for (int i = 0; i < bCharList1.size(); i++) {
-            if (bStateList1.get(i) == 0 && !filter.containsKey(i)){
-                filter.put(bCharList1.get(i),i);
-            }
-            else if(bStateList1.get(i) == 0){
+            if (bStateList1.get(i) == 0 && !filter.containsKey(i)) {
+                filter.put(bCharList1.get(i), i);
+            } else if (bStateList1.get(i) == 0) {
                 flagList1[i] = 1;
-            }
-            else if (bStateList1.get(i) == 1 && filter.containsKey(i)){
+            } else if (bStateList1.get(i) == 1 && !filter.containsKey(i)) {
                 filter.remove(bCharList1.get(i));
-            }
-            else if(bStateList1.get(i)==1){
+            } else if (bStateList1.get(i) == 1) {
                 flagList1[i] = 1;
             }
         }
         for (int i = flagList1.length - 1; i >= 0; i--) {
-            if (flagList1[i] == 1){
+            if (flagList1[i] == 1) {
                 bCharList1.remove(i);
                 bTimeList1.remove(i);
                 bStateList1.remove(i);
@@ -219,33 +168,75 @@ public class Postman {
         filter.clear();
         int[] flagList2 = new int[bCharList2.size()];
         for (int i = 0; i < bCharList2.size(); i++) {
-            if (bStateList2.get(i) == 0 && !filter.containsKey(i)){
-                filter.put(bCharList2.get(i),i);
-            }
-            else if(bStateList2.get(i) == 0){
+            if (bStateList2.get(i) == 0 && !filter.containsKey(i)) {
+                filter.put(bCharList2.get(i), i);
+            } else if (bStateList2.get(i) == 0) {
                 flagList2[i] = 1;
-            }
-            else if (bStateList2.get(i) == 1 && filter.containsKey(i)){
+            } else if (bStateList2.get(i) == 1 && !filter.containsKey(i)) {
                 filter.remove(bCharList2.get(i));
-            }
-            else if(bStateList2.get(i)==1){
+            } else if (bStateList2.get(i) == 1) {
                 flagList2[i] = 1;
             }
         }
         for (int i = flagList2.length - 1; i >= 0; i--) {
-            if (flagList2[i] == 1){
+            if (flagList2[i] == 1) {
                 bCharList2.remove(i);
                 bTimeList2.remove(i);
                 bStateList2.remove(i);
             }
         }
+        System.out.println("aCharList = ");
+        for (int i : aCharList
+                ) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\naStateList = ");
+        for (int i : aStateList
+                ) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\naTimeList = ");
+        for (int i : aTimeList
+                ) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nbCharList1 = ");
+        for (int i : bCharList1
+                ) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nbStateList1 = ");
+        for (int i : bStateList1
+                ) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nbTimeList1 = ");
+        for (int i : bTimeList1
+                ) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nbCharList2 = ");
+        for (int i : bCharList2
+                ) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nbStateList2 = ");
+        for (int i : bStateList2
+                ) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nbTimeList2 = ");
+        for (int i : bTimeList2
+                ) {
+            System.out.print(i + " ");
+        }
         try {
-            PeopleText p1 = new PeopleText(aCharList,aStateList,aTimeList);
-            PeopleText p2 = new PeopleText(bCharList1,bStateList1,bTimeList1);
-            PeopleText p3 = new PeopleText(bCharList2,bStateList2,bTimeList2);
-            PeopleText[] list = {p2,p3};
+            PeopleText p1 = new PeopleText(aCharList, aStateList, aTimeList);
+            PeopleText p2 = new PeopleText(bCharList1, bStateList1, bTimeList1);
+            PeopleText p3 = new PeopleText(bCharList2, bStateList2, bTimeList2);
+            PeopleText[] list = {p2, p3};
             TextJudger tj = new TextJudger();
-            return tj.compare(list,p1);
+            return tj.compare(list, p1);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
             return 0.79f;
